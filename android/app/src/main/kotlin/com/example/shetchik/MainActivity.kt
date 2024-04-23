50081bae23b4312adf74985f44ffcdf1d45b8b65
Igdfkg <ggggo0@yandex.ru>
2024-04-23 10:48:00 +0300

    1
    1

---
+++ android/app/src/main/kotlin/com/example/shetchik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shetchik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CounterApplication());
+
+class CounterApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
